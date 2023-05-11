eead66278c38960520b963e655326eb7d8c05092
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-05-11 16:00:26 +0200

    searchbar

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
-const maxPage = 42; //make it dynamic later
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
-async function fetchCharacters(page){
-  const response= await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+async function fetchCharacters(page, searchQuery){
+  const response= await fetch(`https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`);
   const data= await response.json();
-  // const maxPage= data.info.pages;
-  return data.results;
+  return data;
 };
+
+// States
+let maxPage = (await fetchCharacters(page, searchQuery)).info.pages;
+console.log("this is initial maxPage: ",maxPage);
+
 
 function loopAndAppend(charArray){
   charArray.map(char => {
@@ -29,18 +31,22 @@ function loopAndAppend(charArray){
   });
 };
 
-const initialCharacters= await fetchCharacters(page);
+const fetchedData= await fetchCharacters(page, searchQuery);
+const initialCharacters= fetchedData.results;
 loopAndAppend(initialCharacters);
 pageNumber(page,maxPage)
 
+
+
 nextButton.addEventListener("click", async()=>{
+  maxPage= (await fetchCharacters(page, searchQuery)).info.pages;
   if(page===maxPage){
     return;
   };
   page++;
   cardContainer.innerHTML= "";
-  console.log(page);
-  const characters= await fetchCharacters(page);
+  console.log("maxpage inside button is: ",maxPage);
+  const characters= (await fetchCharacters(page, searchQuery)).results;
   console.log(characters);
   loopAndAppend(characters);
   pageNumber(page, maxPage);
@@ -52,8 +58,8 @@ prevButton.addEventListener("click", async()=>{
   };
   page--;
   cardContainer.innerHTML= "";
-  console.log(page);
-  const characters= await fetchCharacters(page);
+  console.log("this is page number: ", page);
+  const characters= (await fetchCharacters(page, searchQuery)).results;
   console.log(characters);
   loopAndAppend(characters);
   pageNumber(page, maxPage)
@@ -62,3 +68,24 @@ prevButton.addEventListener("click", async()=>{
 function pageNumber(page, maxPage){
   pagination.textContent=`${page} / ${maxPage}`;
 };
+
+
+searchBar.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  searchQuery= e.target.query.value;
+  console.log(searchQuery);
+  cardContainer.innerHTML= "";
+  let searchResult= (await fetchCharacters(page, searchQuery));
+  if(searchResult.includes("error")){
+    console.log("error: 'There is nothing here'");
+    const message= document.createElement("h3");
+    message.textContent= "error: 'There is nothing here'"
+    return cardContainer.append(message);
+  };
+  let searchedCharacter= searchResult.results
+  console.log("search result: ", searchResult);
+  const maxPage= (await fetchCharacters(page, searchQuery)).info.pages;
+  console.log("maxpage inside submit is: ", maxPage);
+ 
+  loopAndAppend(searchedCharacter);
+});
