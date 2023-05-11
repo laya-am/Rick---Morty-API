5ecf8f00d8cf656188b99febe24ba855797c55b0
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-05-11 10:42:29 +0200

    add function to create card

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,27 @@
-export function createCharacterCard() {}
+export function createCharacterCard() {
+    const cardElement= document.createElement("li");
+    cardElement.classList.add("card");
+    cardElement.innerHTML=`
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src=${image}
+              alt="Rick Sanchez"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurrencies}</dd>
+            </dl>
+          </div>
+        `;
+        return cardElement;
+}
+ 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import {createCharacterCard} from "./components/card/card.js"
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
-);
+); 
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
